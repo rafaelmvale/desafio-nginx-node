ce825e15dd5f6e9c7a1e48f290cb6f49c4414d6c
Rafael Vale <rafael.vale@refuturiza.com.br>
2024-07-01 08:04:07 -0300

    Desafio finalizado

---
+++ node_app/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'root',
+  database: 'testdb'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+app.get('/', (req, res) => {
+  const name = 'Full Cycle';
+  connection.query('INSERT INTO people (name) VALUES (?)', [name], (err) => {
+    if (err) {
+      console.error('Error inserting into database:', err);
+      res.status(500).send('Server Error');
+      return;
+    }
+
+    connection.query('SELECT name FROM people', (err, results) => {
+      if (err) {
+        console.error('Error fetching from database:', err);
+        res.status(500).send('Server Error');
+        return;
+      }
+
+      let response = '<h1>Full Cycle Rocks!</h1>';
+      response += '<ul>';
+      results.forEach(row => {
+        response += `<li>${row.name}</li>`;
+      });
+      response += '</ul>';
+
+      res.send(response);
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
